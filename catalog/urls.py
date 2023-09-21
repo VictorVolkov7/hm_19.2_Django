e57ed0bc809d1b025e3ffe30779645c18ae219d4
Victor Volkov <vektorn1212@gmail.com>
2023-09-21 22:23:11 +0200

    update urls.py: added path for product page

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 
-from catalog.views import home_page, contact_info
+from catalog.apps import CatalogConfig
+from catalog.views import home_page, contact_info, product_page
+
+app_name = CatalogConfig.name
 
 urlpatterns = [
     path('', home_page, name='home_page'),
-    path('contact/', contact_info, name='contact_info')
-
+    path('contact/', contact_info, name='contact_info'),
+    path('product/<int:pk>/', product_page, name='product_page')
 ]
