8975e1c778a7d269ec4465cf16808740e08cddd0
Victor Volkov <vektorn1212@gmail.com>
2023-09-28 21:06:49 +0200

    update models.py: add Blog model

--- catalog/models.py
+++ catalog/models.py
@@ -43,3 +43,20 @@ class Contacts(models.Model):
     class Meta:
         verbose_name = 'контакт'
         verbose_name_plural = 'контакты'
+
+
+class Blog(models.Model):
+    title = models.CharField(max_length=50, verbose_name='заголовок')
+    slug = models.CharField(max_length=50, verbose_name='slug', **NULLABLE)
+    body = models.TextField(verbose_name='содержимое')
+    preview = models.ImageField(upload_to='blog/', verbose_name='изображение', **NULLABLE)
+    date_create = models.DateField(verbose_name='дата создания')
+    is_published = models.BooleanField(default=True, verbose_name='признак публикации')
+    count_views = models.IntegerField(default=0, verbose_name='количество просмотров')
+
+    def __str__(self):
+        return f'{self.title}'
+
+    class Meta:
+        verbose_name = 'публикация'
+        verbose_name_plural = 'публикации'
