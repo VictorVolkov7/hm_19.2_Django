98cee04f2e9959fd86c9297314667117441054af
Victor Volkov <vektorn1212@gmail.com>
2023-09-28 21:11:08 +0200

    update urls.py: add path for Blog and division by app

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,12 +1,27 @@
 from django.urls import path
 
 from catalog.apps import CatalogConfig
-from catalog.views import home_page, contact_info, product_page
+from catalog.views import contact_info, ProductDetailView, ProductListView, ProductCreateView, BlogListView, \
+    BlogDetailView, BlogCreateView, BlogUpdateView, BlogDeleteView
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
-    path('', home_page, name='home_page'),
     path('contact/', contact_info, name='contact_info'),
-    path('product/<int:pk>/', product_page, name='product_page')
 ]
+
+product_urlpatterns = [
+    path('', ProductListView.as_view(), name='product_list'),
+    path('product/<int:pk>/', ProductDetailView.as_view(), name='product_detail'),
+    path('create/', ProductCreateView.as_view(), name='create_product'),
+]
+
+blog_urlpatterns = [
+    path('blog/', BlogListView.as_view(), name='blog_list'),
+    path('blog/<slug:slug>/', BlogDetailView.as_view(), name='blog_detail'),
+    path('blog/create/', BlogCreateView.as_view(), name='blog_create'),
+    path('blog/edit/<slug:slug>/', BlogUpdateView.as_view(), name='blog_update'),
+    path('blog/delete/<slug:slug>/', BlogDeleteView.as_view(), name='blog_delete'),
+]
+
+urlpatterns += product_urlpatterns + blog_urlpatterns
