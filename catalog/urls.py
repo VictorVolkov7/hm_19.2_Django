77f06d26e42ab80019f018841266717ea410ad47
Victor Volkov <vektorn1212@gmail.com>
2023-10-19 16:49:10 +0200

    update urls.py: add settings for never cached urls and transfer contact controller to CBV

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,28 +1,29 @@
 from django.urls import path
+from django.views.decorators.cache import cache_page, never_cache
 
 from catalog.apps import CatalogConfig
-from catalog.views import contact_info, ProductDetailView, ProductListView, ProductCreateView, BlogListView, \
+from catalog.views import ContactInfo, ProductDetailView, ProductListView, ProductCreateView, BlogListView, \
     BlogDetailView, BlogCreateView, BlogUpdateView, BlogDeleteView, ProductUpdateView, ProductDeleteView
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
-    path('contact/', contact_info, name='contact_info'),
+    path('contact/', ContactInfo.as_view(), name='contact_info'),
 ]
 
 product_urlpatterns = [
     path('', ProductListView.as_view(), name='product_list'),
-    path('product/<int:pk>/', ProductDetailView.as_view(), name='product_detail'),
-    path('product/create/', ProductCreateView.as_view(), name='create_product'),
-    path('product/update/<int:pk>/', ProductUpdateView.as_view(), name='product_update'),
+    path('product/<int:pk>/', cache_page(60)(ProductDetailView.as_view()), name='product_detail'),
+    path('product/create/', never_cache(ProductCreateView.as_view()), name='create_product'),
+    path('product/update/<int:pk>/', never_cache(ProductUpdateView.as_view()), name='product_update'),
     path('product/delete/<int:pk>/', ProductDeleteView.as_view(), name='product_delete'),
 ]
 
 blog_urlpatterns = [
     path('blog/', BlogListView.as_view(), name='blog_list'),
     path('blog/<slug:slug>/', BlogDetailView.as_view(), name='blog_detail'),
-    path('blog/create/', BlogCreateView.as_view(), name='blog_create'),
-    path('blog/edit/<slug:slug>/', BlogUpdateView.as_view(), name='blog_update'),
+    path('blog/create/', never_cache(BlogCreateView.as_view()), name='blog_create'),
+    path('blog/edit/<slug:slug>/', never_cache(BlogUpdateView.as_view()), name='blog_update'),
     path('blog/delete/<slug:slug>/', BlogDeleteView.as_view(), name='blog_delete'),
 ]
 
